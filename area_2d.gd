a84bec919e07eb7fb88141e409558ac5a525ee7a
Renan UFPB <renantomazini@sti.ufpb.br>
2025-11-13 17:42:10 -0300

    primeiro

---
+++ area_2d.gd
@@ -0,0 +1,28 @@
+extends Area2D
+signal coletado
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "raposa":
+		print("aqui")
+		emit_signal("coletado")
+		queue_free()
+	print("entrou")
+	
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.name == "raposa":
+		print("aqui")
+		emit_signal("coletado")
+		queue_free()
+	print("entrou")
---
+++ player.gd
@@ -0,0 +1,54 @@
+extends Area2D
+
+
+@export var speed = 100
+var screen_size
+signal pontua
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	
+
+
+
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("baixo_"):
+		velocity.y += 1	
+		$AnimatedSprite2D.animation = "andar"
+	if Input.is_action_pressed("cima_"):
+		velocity.y -= 1	
+		$AnimatedSprite2D.animation = "andar"
+	if Input.is_action_pressed("esquerda_"):
+		$AnimatedSprite2D.flip_h = true
+		velocity.x -= 1	
+		$AnimatedSprite2D.animation = "andar"
+	if Input.is_action_pressed("direita_"):
+		$AnimatedSprite2D.flip_h = false
+		velocity.x += 1	
+		$AnimatedSprite2D.animation = "andar"
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.animation = "idle"
+		$AnimatedSprite2D.play()
+	position += velocity * delta
+	
+	position.y = clamp(position.y,0, screen_size.y)
+	position.x = clamp(position.x,0, screen_size.x)
+	
+
+	
+
+
+#func _on_body_entered(body: Node2D) -> void:
+#	if body.name == "comida":
+#		emit_signal("pontua")
+#	else:
+#		$AudioStreamPlayer2D.play()
+#	position.x = 603
+#	position.y = 690
